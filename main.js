ed08430b4d247ef94c8ead074914b4ccf40413fb
Kuba Wasiak <qbawasiak@wp.pl>
2017-02-13 17:34:02 +0100

    create-react-app run, new app created

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ const Title2 = () => (
 // always packed in one parent elem f.e. <span>
 ReactDOM.render(
   <span>
-    <h1>Hello world test elo</h1>
+    <h1 id='h1'>Hello world test elo</h1>
     <span>yo yo tu span</span>
   </span>,
   document.getElementById('main-container')
