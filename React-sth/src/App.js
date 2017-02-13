a490c0824eba539667e51dfdbae3fe1fe42bd56c
Kuba Wasiak <qbawasiak@wp.pl>
2017-02-13 19:56:52 +0100

    first use of constructor and state change

--- React-sth/src/App.js
+++ React-sth/src/App.js
@@ -1,5 +1,36 @@
 import React from 'react';
 
-const App = () => <h1>Hi</h1>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state text',
+      cat: 0
+    }
+  }
+
+  update(evt) {
+    this.setState({txt: evt.target.value})
+  }
+
+  render() {
+    // let txt = this.props.txt
+    return (
+      <div>
+        <input type="text" onChange={this.update.bind(this)}/>
+        <h1>{this.state.txt} - {this.state.cat}</h1>
+      </div>
+    )
+  }
+}
+
+// App.propTypes = {
+//   txt: React.PropTypes.string,
+//   cat: React.PropTypes.number.isRequired
+// }
+
+// App.defaultProps = {
+//   txt: "this is default text"
+// }
 
 export default App
